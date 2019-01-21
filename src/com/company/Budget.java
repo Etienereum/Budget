518455bc327b4090337c6d55001e7d8643113a51
Etienne <etienne.oranu@gmail.com>
2019-01-21 17:00:01 +0100

    project set up

---
+++ src/com/company/Budget.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Budget {
+    public double income = 0;
+    public double expenses= 0;
+    public double total= 0;
+
+    public double calculalte(){
+        income += income;
+
+        double bal = income - expenses;
+        return bal;
+    }
+}
